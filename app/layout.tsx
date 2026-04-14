2bd35dcfd3b9b48af79a9a5de402a2279e192b9b
blakemounser24-commits <blakemounser24@gmail.com>
2026-04-14 13:56:02 +1000

    Update page title from 'Torus Shader Animation' to 'glowingleads'

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const _geistMono = V0_Font_Geist_Mono({ subsets: ['latin'], weight: ["100","200"
 const _sourceSerif_4 = V0_Font_Source_Serif_4({ subsets: ['latin'], weight: ["200","300","400","500","600","700","800","900"] })
 
 export const metadata: Metadata = {
-  title: "Torus Shader Animation",
+  title: "glowingleads",
   description: "Black and white torus shader with rotating dots",
   generator: "v0.app",
   icons: {
